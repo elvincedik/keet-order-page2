0674e3d3898e3b641a8874c1a7c625eb712716aa
elvincedik <elvinse3@gmail.com>
2024-10-30 11:39:07 +0100

    keet order page v1

---
+++ assets/js/main.js
@@ -0,0 +1,4 @@
+// mobile navbar toggle feature
+document.getElementById("hamburgerMenu").addEventListener("click", function(){
+    document.getElementById("left-sidebar").classList.toggle("show-left-sidebar");
+})
